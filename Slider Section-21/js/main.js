c9891f79363e481c937c8c91a9a0cec1e6b8fba8
unknown <mehedi.2800@gmail.com>
2018-09-23 00:38:18 +0600

    For Slider Section Update

---
+++ Slider Section-21/js/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function($){
+
+	$('.homepage-slides').owlCarousel({
+		items: 1,
+		dots: true,
+		nav: true,
+		autoplay: false,
+		loop: true,
+	});
+    
+ });
+
+
+$(window).load(function(){
+
+});
