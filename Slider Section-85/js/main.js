0f1a1a03142bf5a2ad9eec6fbe2dbd64dd6046be
mehedihs <mehedi.2800@gmail.com>
2018-10-03 22:44:36 +0600

    update

---
+++ Slider Section-85/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function($) {
+	$(".testimonial-carousel").owlCarousel({
+		items: 1,
+		dots: false,
+		nav: true,
+		navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+
+	});
+});
